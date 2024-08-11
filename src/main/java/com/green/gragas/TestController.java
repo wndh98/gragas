945b4555b7ebd2da96381f78a4d3f788e40409e0
wndh98 <wndh98@naver.com>
2024-08-12 01:11:21 +0900

    first commit

---
+++ src/main/java/com/green/gragas/TestController.java
@@ -0,0 +1,12 @@
+package com.green.gragas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/test")
+    public String hello(){
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/green/gragas/GragasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.gragas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GragasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
