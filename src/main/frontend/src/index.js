0182959e93564e2d8cb3f007fd62c6681371e908
wndh98 <wndh98@naver.com>
2024-08-12 16:00:35 +0900

    test commit2

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./layout/App";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+      <App />
+    <Footer/>
   </React.StrictMode>
 );
 
