f0876cc81d710a8facc698289c310b62acd6e051
pcw614 <pcw8296@gmail.com>
2024-08-26 14:15:20 +0900

    0826

--- src/main/java/com/green/gragas/subscribe/controller/SubscripbeOrderController.java
+++ src/main/java/com/green/gragas/subscribe/controller/SubscripbeOrderController.java
@@ -4,15 +4,15 @@ import com.green.gragas.subscribe.dto.SubscribeOrder;
 import com.green.gragas.subscribe.service.SubscribeOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SubscripbeOrderController {
     @Autowired
     public SubscribeOrderService sos;
-    @PostMapping("/subsribe/subsOrder/regist")
-    public int insertOrderInfo(@RequestPart("subscribeOrder")SubscribeOrder subscribeOrder){
+    @PostMapping("/subscribe/subsOrder/regist")
+    public int insertOrderInfo(@RequestBody SubscribeOrder subscribeOrder){
         int result = sos.insertOrderInfo(subscribeOrder);
         return result;
     }
