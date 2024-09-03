76c5a87c93ee8f21d9c1d0a54f69cd66f2d0a090
wndh98 <wndh98@naver.com>
2024-09-04 08:29:28 +0900

    0902

--- src/main/frontend/src/layout/AdminHeader.jsx
+++ src/main/frontend/src/layout/AdminHeader.jsx
@@ -1,10 +1,38 @@
 import { Link } from "react-router-dom";
 
-function AdminHeader(){
-    return(
-       <header>
-            어드민헤더입니다.
-       </header>
+function AdminHeader() {
+    return (
+        <div className="container">
+            <header className="d-flex justify-content-center py-3">
+                <ul className="nav nav-pills ">
+                    <li className="nav-item">
+                        <button className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">회원관리</button>
+                        <ul class="dropdown-menu">
+                            <li><Link className="dropdown-item" to="">회원관리</Link></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">상품관리</button>
+                        <ul class="dropdown-menu">
+                            <li><Link className="dropdown-item" to="">상품관리</Link></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">구독관리</button>
+                        <ul class="dropdown-menu">
+                            <li><Link className="dropdown-item" to="">구독관리</Link></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">주문관리</button>
+                        <ul class="dropdown-menu">
+                            <li><Link className="dropdown-item" to="">주문관리</Link></li>
+                        </ul>
+                    </li>
+                </ul>
+            </header>
+        </div>
+
     );
 }
 export default AdminHeader;
--- src/main/frontend/src/layout/Footer.jsx
+++ src/main/frontend/src/layout/Footer.jsx
@@ -1,10 +1,20 @@
-function Footer(){
+import { Link } from "react-router-dom";
 
-    return(
-       <footer className="fs-5 text-center" style={{height:'250px', lineHeight:'250px'}}>
-        <hr />
-        그린 컴퓨터 아카데미 Gragas Project
-       </footer>
+function Footer() {
+
+    return (
+        <div className="container">
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><Link to="#" className="nav-link px-2 text-body-secondary">홈</Link></li>
+                    <li className="nav-item"><Link to="#" className="nav-link px-2 text-body-secondary">상품</Link></li>
+                    <li className="nav-item"><Link to="#" className="nav-link px-2 text-body-secondary">구독</Link></li>
+                    <li className="nav-item"><Link to="#" className="nav-link px-2 text-body-secondary">게시판</Link></li>
+                    <li className="nav-item"><Link to="#" className="nav-link px-2 text-body-secondary">이벤트</Link></li>
+                </ul>
+                <p className="text-center text-body-secondary">그린컴퓨터아카데미</p>
+            </footer>
+        </div>
     );
 }
 export default Footer;
